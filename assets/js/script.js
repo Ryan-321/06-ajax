9629109c74303924236fa021146275b1d0604a75
Ryan Kahle <jrkahle@hotmail.com>
2017-06-13 13:58:13 -0400

    functionality complete

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,41 +8,64 @@ $(document).ready(function() {
     for (var i = 0; i < topics.length; i++) {
       var $button = $('<button/>')
         .addClass('btn')
-        .attr('data-name', topics[i])
+        .attr('data-name', topics[i]+'+simpson')
         .text(topics[i])
       $('#button-wrapper').append($button)
     }
   }
 
-
   function getImages () {
     var name = $(this).attr('data-name')
-    var queryURL = 'http://api.giphy.com/v1/gifs/search?q=' + name + '&limit=10&api_key=dc6zaTOxFJmzC'
+    var queryURL = 'http://api.giphy.com/v1/gifs/search?q=' + name + '&limit=12&api_key=dc6zaTOxFJmzC'
+    console.log(queryURL)
     $.ajax({
       url: queryURL,
       method: 'GET'
     }).done(function (res) {
       $('#image-wrapper').empty()
-      res.data.forEach(function(item) {
-      console.log(item)
-      var $img = $('<img/>')
-        .attr('src', item.images.original_still.url)
-        .attr('data-still', item.images.original_still.url)
-        .attr('data-giphy', item.images.original.url)
-        .addClass('image')
-      $('#image-wrapper').append($img)
-      })
+      createImage(res)
     })
   }
-   function clickImage () {
-     var src = $(this).attr('src')
-     var still = $(this).attr('data-still')
-     var giphy = $(this).attr('data-giphy')
-     src === still ? $(this).attr('src', giphy) : $(this).attr('src', still)
-   }
+
+  function clickImage () {
+    var src = $(this).attr('src')
+    var still = $(this).attr('data-still')
+    var giphy = $(this).attr('data-giphy')
+    src === still ? $(this).attr('src', giphy) : $(this).attr('src', still)
+  }
+
+  function createImage (res) {
+    res.data.forEach(function(item) {
+    // create img element
+    var $img = $('<img/>')
+      .attr('src', item.images.original_still.url)
+      .attr('data-still', item.images.original_still.url)
+      .attr('data-giphy', item.images.original.url)
+      .addClass('image')
+    // create p element
+    var $p = $('<p/>')
+      .text(item.rating.toUpperCase())
+      .addClass('rating')
+    // create div to hold $img and $p
+    var $div = $('<div/>')
+      .append($img, $p)
+      .addClass('giphy-wrapper')
+    $('#image-wrapper').append($div)
+    })
+  }
+
+  function addTopics (e) {
+    e.preventDefault()
+    var newGif = $('#new-giphy-input').val()
+    topics.push(newGif)
+    addButtons()
+  }
+
   // Adds onClick higher up in DOM heirarchy.
   // Solves adding event listener to element newly created
   $(document).on('click', '.btn', getImages)
   $(document).on('click', '.image', clickImage)
+  $(document).on('click', '#add-topic', addTopics)
+
   addButtons()
 })
